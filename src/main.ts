6e097f59958c28c37284037b1ae0d3a2ee31afff
Cemil Colak <cemil@colak.cc>
2024-10-26 16:54:08 +0300

    created swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
+
+    const config = new DocumentBuilder()
+        .setTitle('Movie Management API')
+        .setDescription('The movie management API description')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document); // 'api/docs' route for Swagger documentation
+
     await app.listen(process.env.PORT ?? 3000);
     console.log('Application is running on: http://localhost:3000/api');
 }
