51dc9a61c506051563f6413181a30e69d2db7927
Cemil Colak <cemil@colak.cc>
2024-10-26 17:04:37 +0300

    updated create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,16 +1,20 @@
 import {IsNotEmpty, IsString, MinLength, IsInt, Min} from 'class-validator';
 import {UserRole} from '../entities/user.entity';
+import {ApiProperty} from "@nestjs/swagger";
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     username: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     @MinLength(6)
     password: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsInt()
     @Min(13)
